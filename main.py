b44341c195de706ee7465173423ab3309cfc63e9
IvEn-04 <Strevs4@yandex.ru>
2026-08-05 21:05:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-# Присвоение переменной числового значения.
-favorite_number = (11)
-print(favorite_number)
-# Написание числовых операций с последующим заключением в команде print. 
-print(4+4)
-print(16-8)
-print(1*8)
-print(int(80/10))
+name = "Ivan"
+print(name)
